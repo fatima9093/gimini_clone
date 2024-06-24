58b4cace584d76e881fc27467f82e01dfef01239
Muhammad Hamid <fatimaahmad9093@gmail.com>
2024-06-24 16:08:37 +0500

    gimini

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Sidebar from './Component/Sidebar/Sidebar'
+import Main from './Component/Main/Main'
+
+function App() {
+
+  return (
+    <>
+      <Sidebar/>
+      <Main />
+    </>
+  )
+}
+
+export default App
